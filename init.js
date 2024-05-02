4ab11bdd1a82ac833f504c02eda6e69a6474857c
requiempt <145904266+requiempt@users.noreply.github.com>
2024-05-02 20:28:50 +0300

    Update init.js

--- init.js
+++ init.js
@@ -1,4 +1,20 @@
+$(function(){
 
+  var $w = $(window),
+      $background = $('#loader');
+
+  // Fix background image jump on mobile
+  if ((/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera)) {
+    $background.css({'top': 'auto', 'bottom': 0});
+
+    $w.resize(sizeBackground);
+    sizeBackground();
+  }
+
+  function sizeBackground() {
+     $background.height(screen.height);
+  }
+});
 
 
 const navigation = document.querySelector(".primary-navigation");
